a3897dad5e7c471dfeb64896d5b47380bfdb9d97
Mootassam <mootassame@gmail.com>
2022-06-05 02:12:32 +0100

    show detaill quesiton when clik into the question

--- index.js
+++ index.js
@@ -10,3 +10,15 @@ const show = (evt, idName) => {
   document.getElementById(idName).style.display = "flex";
   evt.currentTarget.className += " active";
 };
+let question = document.getElementsByClassName("question");
+let answer = document.getElementsByClassName("answer");
+let image = document.getElementsByClassName("image");
+const active = (event, idName) => {
+  for (i = 0; i < answer.length; i++) {
+    answer[i].style.display = "none";
+  }
+  for (let index = 0; index < question.length; index++) {
+    // console.log(question[index]);
+  }
+  document.getElementById(idName).style.display = "flex";
+};
